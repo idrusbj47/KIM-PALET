b13ec2250dd07608e3b2d77303910642c2ac92b7
idrusbj47 <idrusbj47@gmail.com>
2026-07-06 15:21:33 +0700

    Create sw.js
    logo web

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'pallet-app-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './logo1.png'
+];
+
+// Pemasangan Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Aktivasi & Pembersihan Cache Lama
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Ambil data dari cache jika offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
